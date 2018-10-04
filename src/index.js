e1e7ff5bbcb77b8ed4494c9237788c6f86298d7a
ryan hs <mr.ryansilalahi@gmail.com>
2018-10-04 14:49:09 +0700

    update versioning

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 
+global.IS_DEBUG = process.env.NODE_ENV !== 'production'
+
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
@@ -17,11 +20,21 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const GRAPHQL_PLAYGROUND_CONFIG = {
+  settings: {
+    'editor.cursorShape': 'line',
+    'editor.fontSize': 14,
+    'editor.reuseHeaders': true,
+    'editor.theme': 'light'
+  }
+};
+
+const server = new ApolloServer({ playground: IS_DEBUG ? GRAPHQL_PLAYGROUND_CONFIG : false, typeDefs, resolvers });
+
 
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-);
+app.listen({ port: process.env.APP_PORT }, () => {
+  console.log(`🚀 Server ready at port ${process.env.APP_PORT}, and graphql on ${server.graphqlPath}`)
+});
