d7504b8e1a2b6dc757bd97561bdd289f7c0686d7
ryanhs <ryan.hamonangansilalahi@ai.astra.co.id>
2019-09-12 15:20:56 +0700

    add mysql option to be enable or no

--- src/data/index.js
+++ src/data/index.js
@@ -14,17 +14,19 @@ global.data = { mysql: null, redis: null, redisLock: null, redisRedlock: null };
 
 const bootstrap = async () => {
   // mysql pool
-  global.data.mysql =  mysql.createPool({
-    host: process.env.APP_MYSQL_HOSTNAME,
-    user: process.env.APP_MYSQL_USERNAME,
-    password: process.env.APP_MYSQL_PASSWORD,
-    database: process.env.APP_MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 2,
-    queueLimit: 0,
-    Promise
-  }).promise();
-  global.data.mysql.on('connection', () => log.info('Mysql connected!'));
+  if (process.env.APP_ACTIVE_MYSQL === '1') {
+    global.data.mysql =  mysql.createPool({
+      host: process.env.APP_MYSQL_HOSTNAME,
+      user: process.env.APP_MYSQL_USERNAME,
+      password: process.env.APP_MYSQL_PASSWORD,
+      database: process.env.APP_MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 2,
+      queueLimit: 0,
+      Promise
+    }).promise();
+    global.data.mysql.on('connection', () => log.info('Mysql connected!'));
+  }
 
   // redis
   if (process.env.APP_ACTIVE_REDIS === '1') {
